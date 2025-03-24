34fa520a90cbad8cdcc96f79dc2194433ff1222c
pedroEEP <pemilioh@gmail.com>
2025-03-24 11:31:01 +0100

    Añadiendo nuevo archivo

---
+++ pruebaBBDD2.py
@@ -0,0 +1,64 @@
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
+
+def crear():
+    try:
+        miconexion=sqlite3.connect("BBDD10")
+        micursor=miconexion.cursor()
+        micursor.execute("""CREATE TABLE alumnos
+                     (id integer primary key autoincrement,
+                     usuario varchar(20),
+                     passwd varchar(20))""")
+        miconexion.commit()
+        miconexion.close()
+        messagebox.showinfo("BBDD","Base de datos creada con éxito")     
+    except:
+        messagebox.showinfo("BBDD","Base de Datos ya existe")
+        
+def insertar():
+    miconexion=sqlite3.connect("BBDD10")
+    micursor=miconexion.cursor()
+    micursor.execute("""INSERT INTO alumnos
+                     (usuario, passwd)VALUES(?,?)""",
+                     (contenido_nombre.get(),
+                      contenido_passwd.get()))   
+    miconexion.commit()
+    miconexion.close()
+    messagebox.showinfo("BBDD","Registro insertado con éxito")
+root=Tk()
+
+#variables
+contenido_id=StringVar()
+contenido_nombre=StringVar()
+contenido_passwd=StringVar()
+
+mi_frame=Frame(root)
+mi_frame.pack()
+
+label_id=Label(mi_frame,text="id: ")
+label_id.grid(row=0,column=0,padx=10,pady=10)
+
+entry_id=Entry(mi_frame, textvariable=contenido_id)
+entry_id.grid(row=0,column=1,padx=10,pady=10)
+
+label_nombre=Label(mi_frame,text="Nombre: ")
+label_nombre.grid(row=1,column=0,padx=10,pady=10)
+
+entry_nombre=Entry(mi_frame, textvariable=contenido_nombre)
+entry_nombre.grid(row=1,column=1,padx=10,pady=10)
+
+label_passwd=Label(mi_frame,text="Password: ")
+label_passwd.grid(row=2,column=0,padx=10,pady=10)
+
+entry_passwd=Entry(mi_frame, textvariable=contenido_passwd, show=("*"))
+entry_passwd.grid(row=2,column=1,padx=10,pady=10)
+
+boton_crear=Button(mi_frame,text="Crear BBDD", command=crear)
+boton_crear.grid(row=3,column=0,padx=10,pady=10)
+
+boton_insertar=Button(mi_frame,text="Insertar BBDD", command=insertar)
+boton_insertar.grid(row=3,column=1,padx=10,pady=10)
+
+
+root.mainloop()
