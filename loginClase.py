f94b3dc21994f1187c4632feba6f871f78af0631
pedroEEP <pemilioh@gmail.com>
2025-04-04 12:33:37 +0200

    Añadiendo nuevo archivo

--- loginClase.py
+++ loginClase.py
@@ -2,23 +2,27 @@ from tkinter import *
 from tkinter import messagebox
 import sqlite3
 import hashlib
+import os
 
 def encriptar_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
 def logearse():
-    mi_conexion=sqlite3.connect("BBDD11")
+    mi_conexion = sqlite3.connect("BBDD11")
     mi_cursor=mi_conexion.cursor()
     mi_cursor.execute("""SELECT * FROM alumnos
                       WHERE usuario=? AND passwd=?""",
                       (contenido_usuario.get(),encriptar_password(contenido_passwd.get())))
     resultado=mi_cursor.fetchall()
-    print(resultado)
+    #print(resultado)
     if resultado:
         messagebox.showinfo("Usario","Login correcto")
-    
+        root.destroy()   
+        #os.system("python sumaClase.py")  
+        #os.startfile("Practica.pdf")
+        #os.startfile("C:\\Users\\pemil\\Desktop\\BrowserSqlite.msi")
+        os.startfile("sumaClase.exe")
     mi_conexion.close()
-
 
 root=Tk()
 
@@ -34,7 +38,7 @@ entry_usuario.grid(row=0, column=1, pady=10,padx=10)
 label_password=Label(root, text="Password")
 label_password.grid(row=1, column=0, pady=10,padx=10)
 
-entry_password=Entry(root,textvariable=contenido_passwd)
+entry_password=Entry(root,textvariable=contenido_passwd, show=("*"))
 entry_password.grid(row=1, column=1, pady=10,padx=10)
 
 boton_login=Button(root,text="Login", command=logearse)
