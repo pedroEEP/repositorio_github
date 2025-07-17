9715c386f359e878fc2fb50b04235a11bab0513a
pedroEEP <pemilioh@gmail.com>
2025-07-17 10:30:43 +0200

    Añadiendo nuevo archivo

---
+++ prueba1.py
@@ -0,0 +1,20 @@
+milista=["Maria", "Pepe", "Marta", "Antonio"]
+
+milista.append("Hacer la compra", "Estudiar Python", "Limpiar la casa")
+milista.append(["Hacer la compra", "Estudiar Python", "Limpiar la casa"])
+print(milista)
+
+print(milista[4][0])
+print (milista[2])
+
+
+
+
+
+
+
+
+
+
+
+
