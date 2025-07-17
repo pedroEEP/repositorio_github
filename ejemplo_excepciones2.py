a1faa901d2bfe905ff4ad1ffb07b7a77ab5030a2
pedroEEP <pemilioh@gmail.com>
2025-07-17 10:22:08 +0200

    Eliminando archivos del repositorio excepto .gitignore

--- ejemplo_excepciones2.py
+++
@@ -1,7 +0,0 @@
-def divide():
-	op1=(float(input("Introduce el primer numero: ")))
-	op2=(float(input("Introduce el segundo numero: ")))
-
-	print("La division es: " +str(op1/op2))
-	print("Termina el cálculo")
-divide()
